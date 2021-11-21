c39f9831b8ab2672b2fa8d95e171a58eb628e3ae
miyazakimaiko <myzkmik19922@gmail.com>
2021-11-21 14:46:08 +0000

    Import recipe module

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const app = module.exports = express();
 const port = process.env.PORT || 3001;
 
 require("./routes/beans.js")(app);
+require("./routes/recipes.js")(app);
 
 app.listen(port, () => {
     console.log(`Server is up and listening on port http://localhost:${port}/`);
