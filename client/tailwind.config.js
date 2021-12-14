9406de168a84127800387856a0b2d102ab3ead44
miyazakimaiko <myzkmik19922@gmail.com>
2021-12-14 16:29:31 +0000

    new file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'burnt-sienna': '#66615B',
+        'creme': '#F4F3EE',
+      },
+      fontFamily: {
+        'capitals': ['Sen', ...defaultTheme.fontFamily.sans],
+        'sans': ['Nunito', ...defaultTheme.fontFamily.sans]
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      ringWidth: ['focus-visible'],
+      ringColor: ['focus-visible'],
+      ringOffsetWidth: ['focus-visible'],
+      ringOffsetColor: ['focus-visible'],
+    },
   },
   plugins: [],
 }
