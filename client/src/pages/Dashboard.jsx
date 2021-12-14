b66f90310fb5d3ce69887f51dca170a87dd84191
miyazakimaiko <myzkmik19922@gmail.com>
2021-12-14 20:50:57 +0000

    add components

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Header from '../shared/Header'
+import ChartBarBeans from './dashboard-component/ChartBarBeans'
+import ChartBarRecipes from './dashboard-component/ChartBarRecipes'
+import TotalBeans from './dashboard-component/TotalBeans'
+import TotalBrews from './dashboard-component/TotalBrews'
+import TotalRecipes from './dashboard-component/TotalRecipes'
 
 const Dashboard = () => {
   return (
     <>
       <Header/>
-      Dashboard
+      <div className="flex mb-4">
+        <TotalBrews />
+        <TotalRecipes />
+        <TotalBeans />
+      </div>
+      <div className="flex mb-4">
+        <ChartBarBeans />
+        <ChartBarRecipes />
+      </div>
     </>
   )
 }
