920c8ea122c33862a7acd1049f5edf82b1a7ad7f
miyazakimaiko <myzkmik19922@gmail.com>
2021-12-21 23:00:05 +0000

    Implement error handling

--- server/app.js
+++ server/app.js
@@ -1,10 +1,30 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 const app = module.exports = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 4001;
 
+app.use(morgan('dev'));
+
+require("./routes/register.js")(app);
+require("./routes/users.js")(app);
 require("./routes/beans.js")(app);
 require("./routes/recipes.js")(app);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
 
 app.listen(port, () => {
     console.log(`Server is up and listening on port http://localhost:${port}/`);
