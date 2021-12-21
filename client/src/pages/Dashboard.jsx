a842ddb93c34cbaacc07c4c8a14d19bf0a23a7f9
miyazakimaiko <myzkmik19922@gmail.com>
2021-12-21 23:00:41 +0000

    Remove world map

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Header from '../shared/Header'
 import ChartBarBeans from './dashboard-component/ChartBarBeans'
 import ChartBarRecipes from './dashboard-component/ChartBarRecipes'
-import ChartWorldMap from './dashboard-component/ChartWorldMap'
 import TotalBeans from './dashboard-component/TotalBeans'
 import TotalBrews from './dashboard-component/TotalBrews'
 import TotalRecipes from './dashboard-component/TotalRecipes'
 
 const Dashboard = () => {
+  const title = "Dashboard";
   return (
     <>
-      <Header/>
+      <Header title={title}/>
       <div className="flex mb-4">
         <TotalBrews />
         <TotalRecipes />
