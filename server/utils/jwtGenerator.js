c7e33f039775b45d65c4a235f463b5749a428b26
miyazakimaiko <myzkmik19922@gmail.com>
2021-12-25 17:04:30 +0000

    Fix nesting issue

--- server/utils/jwtGenerator.js
+++ server/utils/jwtGenerator.js
@@ -3,7 +3,9 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (userid) => {
   const payload = {
-    user: userid
+    user: {
+      user_id: userid
+    }
   }
   return jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: "1hr"})
 }
