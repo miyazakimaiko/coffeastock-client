52e335809bd65a1203c53440d0fed0d4f10b9b04
miyazakimaiko <myzkmik19922@gmail.com>
2022-01-11 00:46:13 +0000

    Sort the custom range object items

--- client/src/context/CustomRange.jsx
+++ client/src/context/CustomRange.jsx
@@ -4,9 +4,21 @@ import { createContext } from 'react'
 export const CustomRangeContext = createContext();
 
 const CustomRange = (props) => {
-  const [customRange, setCustomRange] = useState([])
+  const [customRange, setCustomRange] = useState({})
+
+  const addCustomRange = (newRange) => {
+    const orderedRange = Object.keys(newRange).sort().reduce(
+      (obj, key) => { 
+        obj[key] = newRange[key]; 
+        return obj;
+      }, 
+      {}
+    );
+    setCustomRange(orderedRange)
+  }
+  
   return (
-    <CustomRangeContext.Provider value={{customRange, setCustomRange}}>
+    <CustomRangeContext.Provider value={{customRange, setCustomRange, addCustomRange}}>
       {props.children}
     </CustomRangeContext.Provider>
   )
