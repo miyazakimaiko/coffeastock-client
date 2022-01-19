5f70ba6c4a13e9c42e15bab09a6506664b99b7b9
miyazakimaiko <myzkmik19922@gmail.com>
2022-01-19 23:38:16 +0000

    use createRef

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useEffect, useContext, createRef } from 'react';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
@@ -19,8 +19,15 @@ import FloatingMenu from './shared/FloatingMenu';
 import SettingsCustomRange from './pages/SettingsCustomRange';
 import { AccountContext } from './context/Account';
 import CustomRange from './context/CustomRange';
+import Header from './shared/Header';
+import NavState from './context/NavState';
 
 const App = () => {
+  const mainRef = createRef();
+  const navRef = createRef();
+  const headerRef = createRef();
+  const pushpinRef = createRef();
+
 
   const { getSession, setUserData, authenticated, setAuthenticated } = useContext(AccountContext);
 
@@ -28,46 +35,47 @@ const App = () => {
     getSession().then((session) => {
       setUserData(session);
       setAuthenticated(true);
-    });
+    }); 
   }, []);
 
   return <>
     <CustomRange>
-      <div className="relative flex min-h-screen bg-creme font-sans">
+      <NavState>
+        <div className="relative flex flex-col min-h-screen bg-creme font-sans">
           <Router>
-            <div className="flex w-full">
-              {authenticated ? <Nav />: ""}
-              <div className={"w-full box-border text-sm text-burnt-sienna" + (authenticated ? " pl-64" : "")}>
-                <Routes>
-                <Route exact path='/login' element={authenticated ? <Navigate to="/" /> : <Login />} />
-                <Route exact path='/register' element={authenticated ? <Navigate to="/" /> : <Register />} />
+            {authenticated ? <Nav navRef={navRef} mainRef={mainRef} headerRef={headerRef} pushpinRef={pushpinRef}/>: ""}
+            <div ref={mainRef} className={"main w-full box-border text-sm text-burnt-sienna"}>
+              {authenticated ? <Header title="Header" mainRef={mainRef} navRef={navRef} pushpinRef={pushpinRef}/>: ""}
+              <Routes>
+              <Route exact path='/login' element={authenticated ? <Navigate to="/" /> : <Login />} />
+              <Route exact path='/register' element={authenticated ? <Navigate to="/" /> : <Register />} />
 
-                <Route exact path='/' element={authenticated ? <Dashboard /> : <Navigate to="/login" />} />
-                <Route exact path="/coffees" element={authenticated ? <ViewMyCoffees /> : <Navigate to="/login" />} />
-                <Route exact path="/recipes/:roaster_id/:coffee_id" element={authenticated ? <ViewRecipes /> : <Navigate to="/login" />} />
-                <Route exact path="/add/bean" element={authenticated ? <AddCoffeeBean /> : <Navigate to="/login" />} />
-                <Route exact path="/add/recipe" element={authenticated ? <AddRecipe /> : <Navigate to="/login" />} />
-                <Route exact path="/edit/:bean_id" element={authenticated ? <EditCoffeeBean /> : <Navigate to="/login" />} />
-                <Route exact path="/edit/:recipe_id" element={authenticated ? <EditRecipe /> : <Navigate to="/login" />} />
+              <Route exact path='/' element={authenticated ? <Dashboard /> : <Navigate to="/login" />} />
+              <Route exact path="/coffees" element={authenticated ? <ViewMyCoffees /> : <Navigate to="/login" />} />
+              <Route exact path="/recipes/:roaster_id/:coffee_id" element={authenticated ? <ViewRecipes /> : <Navigate to="/login" />} />
+              <Route exact path="/add/bean" element={authenticated ? <AddCoffeeBean /> : <Navigate to="/login" />} />
+              <Route exact path="/add/recipe" element={authenticated ? <AddRecipe /> : <Navigate to="/login" />} />
+              <Route exact path="/edit/:bean_id" element={authenticated ? <EditCoffeeBean /> : <Navigate to="/login" />} />
+              <Route exact path="/edit/:recipe_id" element={authenticated ? <EditRecipe /> : <Navigate to="/login" />} />
 
-                <Route exact path="/settings/aroma" element={authenticated ? <SettingsCustomRange parentCat={'Coffee Beans'} cat={'aroma'}/> : <Navigate to="/login" />} />
-                <Route exact path="/settings/farm" element={authenticated ? <SettingsCustomRange parentCat={'Coffee Beans'} cat={'farm'}/> : <Navigate to="/login" />} />
-                <Route exact path="/settings/origin" element={authenticated ? <SettingsCustomRange parentCat={'Coffee Beans'} cat={'origin'}/> : <Navigate to="/login" />} />
-                <Route exact path="/settings/variety" element={authenticated ? <SettingsCustomRange parentCat={'Coffee Beans'} cat={'variety'}/> : <Navigate to="/login" />} />
-                <Route exact path="/settings/process" element={authenticated ? <SettingsCustomRange parentCat={'Coffee Beans'} cat={'process'} /> : <Navigate to="/login" />} />
-                <Route exact path="/settings/roaster" element={authenticated ? <SettingsCustomRange parentCat={'Coffee Beans'} cat={'roaster'} /> : <Navigate to="/login" />} />
+              <Route exact path="/settings/aroma" element={authenticated ? <SettingsCustomRange parentCat={'Coffee Beans'} cat={'aroma'}/> : <Navigate to="/login" />} />
+              <Route exact path="/settings/farm" element={authenticated ? <SettingsCustomRange parentCat={'Coffee Beans'} cat={'farm'}/> : <Navigate to="/login" />} />
+              <Route exact path="/settings/origin" element={authenticated ? <SettingsCustomRange parentCat={'Coffee Beans'} cat={'origin'}/> : <Navigate to="/login" />} />
+              <Route exact path="/settings/variety" element={authenticated ? <SettingsCustomRange parentCat={'Coffee Beans'} cat={'variety'}/> : <Navigate to="/login" />} />
+              <Route exact path="/settings/process" element={authenticated ? <SettingsCustomRange parentCat={'Coffee Beans'} cat={'process'} /> : <Navigate to="/login" />} />
+              <Route exact path="/settings/roaster" element={authenticated ? <SettingsCustomRange parentCat={'Coffee Beans'} cat={'roaster'} /> : <Navigate to="/login" />} />
 
-                <Route exact path="/settings/grinder" element={authenticated ? <SettingsCustomRange parentCat={'Recipes'} cat={'grinder'} /> : <Navigate to="/login" />} />
-                <Route exact path="/settings/method" element={authenticated ? <SettingsCustomRange parentCat={'Recipes'} cat={'method'} /> : <Navigate to="/login" />} />
-                <Route exact path="/settings/palate" element={authenticated ? <SettingsCustomRange parentCat={'Recipes'} cat={'palate'} /> : <Navigate to="/login" />} />
-                <Route exact path="/settings/water" element={authenticated ? <SettingsCustomRange parentCat={'Recipes'} cat={'water'} /> : <Navigate to="/login" />} />
-                </Routes>
-              </div>
+              <Route exact path="/settings/grinder" element={authenticated ? <SettingsCustomRange parentCat={'Recipes'} cat={'grinder'} /> : <Navigate to="/login" />} />
+              <Route exact path="/settings/method" element={authenticated ? <SettingsCustomRange parentCat={'Recipes'} cat={'method'} /> : <Navigate to="/login" />} />
+              <Route exact path="/settings/palate" element={authenticated ? <SettingsCustomRange parentCat={'Recipes'} cat={'palate'} /> : <Navigate to="/login" />} />
+              <Route exact path="/settings/water" element={authenticated ? <SettingsCustomRange parentCat={'Recipes'} cat={'water'} /> : <Navigate to="/login" />} />
+              </Routes>
             </div>
           </Router>
-        {authenticated ? <FloatingMenu />: ""}
-        <ToastContainer theme="colored" hideProgressBar={true}/>
-      </div>
+          {authenticated ? <FloatingMenu />: ""}
+          <ToastContainer theme="colored" hideProgressBar={true}/>
+        </div>
+      </NavState>
     </CustomRange>
   </>
 }
