7d1c12b8ba7234326e0710a855637515205e9efe
miyazakimaiko <myzkmik19922@gmail.com>
2022-03-01 15:42:13 +0000

    Add custom accordion

--- client/src/shared/Nav/index.jsx
+++ client/src/shared/Nav/index.jsx
@@ -1,27 +1,76 @@
-import React, { useState} from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 import { BookOpenIcon, ChevronDownIcon, ChevronUpIcon, CogIcon, HomeIcon } from '@heroicons/react/outline'
-import { useContext } from 'react'
-import './Nav.scss'
-import { NavStateContext } from '../../context/NavState'
 import PushPinIcon from '../../svgs/PushPinIcon'
+import { NavStateContext } from '../../context/NavState'
+import './Nav.scss'
 
 const Nav = (props) => {
   const { showNavbar, pinnedNavbar, setPinnedNavbar } = useContext(NavStateContext);
-  const [openSettingCoffeeBeans, setOpenSettingCoffeeBeans] = useState(false);
-  const [openSettingRecipes, setOpenSettingRecipes] = useState(false);
+  const [openBeansAccordion, innerSetOpenBeansAccordion] = useState(false);
+  const [openRecipesAccordion, innerSetOpenRecipesAccordion] = useState(false);
+
+  const [beansOpacity, setBeansOpacity] = useState(0);
+  const [recipesOpacity, setRecipesOpacity] = useState(0);
+  const [beansMarginTop, setBeansMarginTop] = useState("-400px");
+  const [recipesMarginTop, setRecipesMarginTop] = useState("-400px");
+
+
+  const toggleOpenBeansAccordion = () => {
+    if (openBeansAccordion) {
+      setBeansOpacity(0);
+      innerSetOpenBeansAccordion(false);
+    } else {
+      setBeansMarginTop("0");
+      innerSetOpenBeansAccordion(true);
+    }
+  }
+
+  const toggleOpenRecipesAccordion = () => {
+    if (openRecipesAccordion) {
+      setRecipesOpacity(0);
+      innerSetOpenRecipesAccordion(false);
+    } else {
+      setRecipesMarginTop("0");
+      innerSetOpenRecipesAccordion(true);
+    }
+  }
+
+  useEffect(() => {
+    if (!openBeansAccordion) {
+      const beansAccordionTimer = setTimeout(() => setBeansMarginTop("-400px"), 200);
+      return() => clearTimeout(beansAccordionTimer)
+    } else {
+      const beansAccordionTimer = setTimeout(() => setBeansOpacity(1), 300);
+      return() => clearTimeout(beansAccordionTimer)
+    }
+  }, [openBeansAccordion]);
+
+  useEffect(() => {
+    if (!openRecipesAccordion) {
+      const recipesAccordionTimer = setTimeout(() => setRecipesMarginTop("-400px"), 200);
+      return() => clearTimeout(recipesAccordionTimer)
+    } else {
+      const recipesAccordionTimer = setTimeout(() => setRecipesOpacity(1), 300);
+      return() => clearTimeout(recipesAccordionTimer)
+    }
+  }, [openRecipesAccordion]);
+
 
   return (
-  <div id="nav-bar">
+  <div>
     <nav
       ref={el => { props.navRef.current = el; }}
-      id="nav"
       className="l-nav fixed top-0 bottom-0 overflow-auto
-        bg-burnt-sienna border-r text-white"
-    >
-      <div className=" border-b border-creme border-opacity-20 h-16 px-6 flex justify-between items-center">
+        bg-burnt-sienna border-r text-white">
+      <div 
+        className=" border-b border-creme 
+        border-opacity-20 h-16 px-6 flex justify-between items-center">
         <h1 className="uppercase text-lg font-capitals">Coffee Journal</h1>
-        <button type="button" ref={el => { props.pushpinRef.current = el; }} className="h-4 w-4 pushpin hidden lg:block" onClick={setPinnedNavbar}>
+        <button 
+          type="button" 
+          ref={el => { props.pushpinRef.current = el; }} 
+          className="h-4 w-4 pushpin hidden lg:block" onClick={setPinnedNavbar}>
           <PushPinIcon />
         </button>
       </div>
@@ -35,8 +84,7 @@ const Nav = (props) => {
             <NavLink to="/"
               className={({ isActive }) => "flex items-center" 
                 + (isActive ? ' nav-link-active font-bold' : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
-                onClick={pinnedNavbar ? null : showNavbar}
-            >
+                onClick={pinnedNavbar ? null : showNavbar}>
               <HomeIcon className="h-4 w-4"></HomeIcon>
               <span className="ml-4">Dashboard</span>
             </NavLink>
@@ -46,8 +94,7 @@ const Nav = (props) => {
             <NavLink exact="true" to="coffees"
               className={({ isActive }) => "flex items-center" 
               + (isActive ? ' nav-link-active font-bold' : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
-              onClick={pinnedNavbar ? null : showNavbar}
-            >
+              onClick={pinnedNavbar ? null : showNavbar}>
               <BookOpenIcon className="h-4 w-4"></BookOpenIcon>
               <span className="ml-4">my coffees</span>
             </NavLink>
@@ -61,156 +108,164 @@ const Nav = (props) => {
         </h3>
         <ul className="ml-6">
           <li className="mr-6">
-            <div className="
-              h-12 flex items-center justify-between
-              transition-opacity duration-300
-              ease-out opacity-70 hover:opacity-100
-              cursor-pointer"
-              onClick={() => setOpenSettingCoffeeBeans(!openSettingCoffeeBeans)}
-              aria-controls="origin-content"
-              aria-expanded={openSettingCoffeeBeans}
-            >
+            <div 
+              className="h-12 flex items-center 
+              justify-between transition-opacity duration-300 
+              ease-out opacity-70 hover:opacity-100 cursor-pointer"
+              onClick={() => toggleOpenBeansAccordion()}>
               <div className="flex items-center">
                 <CogIcon className="h-4 w-4"></CogIcon>
                 <span className="ml-4">For Coffee Beans</span>
               </div>
-
-              { openSettingCoffeeBeans === true ? (
+              { openBeansAccordion === true ? 
                 <ChevronUpIcon className="h-4 w-4"></ChevronUpIcon>
-              ) :
-                <ChevronDownIcon className="h-4 w-4"></ChevronDownIcon>
+              : <ChevronDownIcon className="h-4 w-4"></ChevronDownIcon>
               }
             </div>
-            {/* <Collapse in={openSettingCoffeeBeans}>
-              <div id="origin-content">
-                <ul>
-                  <li className="h-12 ml-8 mr-6 flex items-center justify-between">
-                    <NavLink exact="true" to="settings/origin"
-                      className={({ isActive }) => "flex items-center" 
-                      + (isActive ? ' nav-link-active font-bold' : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
-                      onClick={pinnedNavbar ? null : showNavbar}
-                    >
-                      <span>Origin</span>
-                    </NavLink>
-                  </li>
-                  <li className="h-12 ml-8 mr-6 flex items-center justify-between">
-                    <NavLink exact="true" to="settings/farm"
-                      className={({ isActive }) => "flex items-center" 
-                      + (isActive ? ' nav-link-active font-bold' : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
-                      onClick={pinnedNavbar ? null : showNavbar}
-                    >
-                      <span>Farm</span>
-                    </NavLink>
-                  </li>
-                  <li className="h-12 ml-8 mr-6 flex items-center justify-between">
-                    <NavLink exact="true" to="settings/variety"
-                      className={({ isActive }) => "flex items-center" 
-                      + (isActive ? ' nav-link-active font-bold' : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
-                      onClick={pinnedNavbar ? null : showNavbar}
-                    >
-                      <span>Variety</span>
-                    </NavLink>
-                  </li>
-                  <li className="h-12 ml-8 mr-6 flex items-center justify-between">
-                    <NavLink exact="true" to="settings/process"
-                      className={({ isActive }) => "flex items-center" 
-                      + (isActive ? ' nav-link-active font-bold' : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
-                      onClick={pinnedNavbar ? null : showNavbar}
-                    >
-                      <span>process</span>
-                    </NavLink>
-                  </li>
-                  <li className="h-12 ml-8 mr-6 flex items-center justify-between">
-                    <NavLink exact="true" to="settings/roaster"
-                      className={({ isActive }) => "flex items-center" 
-                      + (isActive ? ' nav-link-active font-bold' : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
-                      onClick={pinnedNavbar ? null : showNavbar}
-                    >
-                      <span>roaster</span>
-                    </NavLink>
-                  </li>
-                  <li className="h-12 ml-8 mr-6 flex items-center justify-between">
-                    <NavLink exact="true" to="settings/aroma"
-                      className={({ isActive }) => "flex items-center" 
-                      + (isActive ? ' nav-link-active font-bold' : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
-                      onClick={pinnedNavbar ? null : showNavbar}
-                    >
-                      <span>aroma</span>
-                    </NavLink>
-                  </li>
-                </ul>
-              </div>
-            </Collapse> */}
+
+            <div className="overflow-hidden">
+              <ul 
+                style={{ marginTop: beansMarginTop, opacity: beansOpacity }}
+                className="ease-in-out transition-all duration-500">
+                <li className="h-12 ml-8 mr-6 flex items-center justify-between">
+                  <NavLink 
+                    exact="true" 
+                    to="settings/origin"
+                    onClick={pinnedNavbar ? null : showNavbar}
+                    className={({ isActive }) => "flex items-center" 
+                    + (isActive ? ' nav-link-active font-bold' 
+                    : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')} >
+                    <span>Origin</span>
+                  </NavLink>
+                </li>
+                <li className="h-12 ml-8 mr-6 flex items-center justify-between">
+                  <NavLink 
+                    exact="true" 
+                    to="settings/farm"
+                    onClick={pinnedNavbar ? null : showNavbar}
+                    className={({ isActive }) => "flex items-center" 
+                    + (isActive ? ' nav-link-active font-bold' 
+                    : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')} >
+                    <span>Farm</span>
+                  </NavLink>
+                </li>
+                <li className="h-12 ml-8 mr-6 flex items-center justify-between">
+                  <NavLink 
+                    exact="true" 
+                    to="settings/variety"
+                    onClick={pinnedNavbar ? null : showNavbar}
+                    className={({ isActive }) => "flex items-center" 
+                    + (isActive ? ' nav-link-active font-bold' 
+                    : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')} >
+                    <span>Variety</span>
+                  </NavLink>
+                </li>
+                <li className="h-12 ml-8 mr-6 flex items-center justify-between">
+                  <NavLink 
+                    exact="true" 
+                    to="settings/process"
+                    onClick={pinnedNavbar ? null : showNavbar}
+                    className={({ isActive }) => "flex items-center" 
+                    + (isActive ? ' nav-link-active font-bold' 
+                    : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')} >
+                    <span>process</span>
+                  </NavLink>
+                </li>
+                <li className="h-12 ml-8 mr-6 flex items-center justify-between">
+                  <NavLink 
+                    exact="true" 
+                    to="settings/roaster"
+                    onClick={pinnedNavbar ? null : showNavbar}
+                    className={({ isActive }) => "flex items-center" 
+                    + (isActive ? ' nav-link-active font-bold' 
+                    : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')} >
+                    <span>roaster</span>
+                  </NavLink>
+                </li>
+                <li className="h-12 ml-8 mr-6 flex items-center justify-between">
+                  <NavLink 
+                    exact="true" 
+                    to="settings/aroma"
+                    onClick={pinnedNavbar ? null : showNavbar}
+                    className={({ isActive }) => "flex items-center" 
+                    + (isActive ? ' nav-link-active font-bold' 
+                    : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')} >
+                    <span>aroma</span>
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
           </li>
 
           <li className="mr-6">
-            <div className="
-              h-12 flex items-center justify-between
-              transition-opacity duration-300
-              ease-out opacity-70 hover:opacity-100
-              cursor-pointer"
-              onClick={() => setOpenSettingRecipes(!openSettingRecipes)}
-              aria-controls="origin-content"
-              aria-expanded={openSettingRecipes}
-            >
+            <div 
+              className="h-12 flex items-center 
+              justify-between transition-opacity duration-300 
+              ease-out opacity-70 hover:opacity-100 cursor-pointer"
+              onClick={() => toggleOpenRecipesAccordion()} >
               <div className="flex items-center">
                 <CogIcon className="h-4 w-4"></CogIcon>
                 <span className="ml-4">For Recipes</span>
               </div>
-
-              { openSettingRecipes === true ? (
+              { openRecipesAccordion === true ? (
                 <ChevronUpIcon className="h-4 w-4"></ChevronUpIcon>
               ) :
                 <ChevronDownIcon className="h-4 w-4"></ChevronDownIcon>
               }
             </div>
-            {/* <Collapse in={openSettingRecipes}>
-              <div id="origin-content">
-                <ul>
-                  <li className="h-12 ml-8 mr-6 flex items-center justify-between">
-                    <NavLink exact="true" to="settings/grinder"
-                      className={({ isActive }) => "flex items-center" 
-                      + (isActive ? ' nav-link-active font-bold' : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
-                      onClick={pinnedNavbar ? null : showNavbar}
-                    >
-                      <span>grinder</span>
-                    </NavLink>
-                  </li>
-                  <li className="h-12 ml-8 mr-6 flex items-center justify-between">
-                    <NavLink exact="true" to="settings/method"
-                      className={({ isActive }) => "flex items-center" 
-                      + (isActive ? ' nav-link-active font-bold' : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
-                      onClick={pinnedNavbar ? null : showNavbar}
-                    >
-                      <span>method</span>
-                    </NavLink>
-                  </li>
-                  <li className="h-12 ml-8 mr-6 flex items-center justify-between">
-                    <NavLink exact="true" to="settings/water"
-                      className={({ isActive }) => "flex items-center" 
-                      + (isActive ? ' nav-link-active font-bold' : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
-                      onClick={pinnedNavbar ? null : showNavbar}
-                    >
-                      <span>water</span>
-                    </NavLink>
-                  </li>
-                  <li className="h-12 ml-8 mr-6 flex items-center justify-between">
-                    <NavLink exact="true" to="settings/palate"
-                      className={({ isActive }) => "flex items-center" 
-                      + (isActive ? ' nav-link-active font-bold' : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
-                      onClick={pinnedNavbar ? null : showNavbar}
-                    >
-                      <span>palate</span>
-                    </NavLink>
-                  </li>
-                </ul>
-              </div>
-            </Collapse> */}
+            <div className="overflow-hidden">
+              <ul 
+                style={{ marginTop: recipesMarginTop, opacity: recipesOpacity }}
+                className="ease-in-out transition-all duration-500">
+                <li className="h-12 ml-8 mr-6 flex items-center justify-between">
+                  <NavLink 
+                    exact="true" 
+                    to="settings/grinder"
+                    className={({ isActive }) => "flex items-center" 
+                    + (isActive ? ' nav-link-active font-bold' 
+                    : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
+                    onClick={pinnedNavbar ? null : showNavbar}>
+                    <span>grinder</span>
+                  </NavLink>
+                </li>
+                <li className="h-12 ml-8 mr-6 flex items-center justify-between">
+                  <NavLink 
+                    exact="true" 
+                    to="settings/method"
+                    className={({ isActive }) => "flex items-center" 
+                    + (isActive ? ' nav-link-active font-bold' 
+                    : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
+                    onClick={pinnedNavbar ? null : showNavbar}>
+                    <span>method</span>
+                  </NavLink>
+                </li>
+                <li className="h-12 ml-8 mr-6 flex items-center justify-between">
+                  <NavLink 
+                    exact="true" 
+                    to="settings/water"
+                    className={({ isActive }) => "flex items-center" 
+                    + (isActive ? ' nav-link-active font-bold' 
+                    : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
+                    onClick={pinnedNavbar ? null : showNavbar}>
+                    <span>water</span>
+                  </NavLink>
+                </li>
+                <li className="h-12 ml-8 mr-6 flex items-center justify-between">
+                  <NavLink 
+                    exact="true" 
+                    to="settings/palate"
+                    className={({ isActive }) => "flex items-center" 
+                    + (isActive ? ' nav-link-active font-bold' 
+                    : ' transition-opacity duration-300 ease-out opacity-70 hover:opacity-100')}
+                    onClick={pinnedNavbar ? null : showNavbar}>
+                    <span>palate</span>
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
           </li>
-
         </ul>
       </div>
-
     </nav>
   </div>
   )
