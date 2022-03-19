b57494d6138704c1ac1e3227134c03058678718b
miyazakimaiko <myzkmik19922@gmail.com>
2022-03-19 12:21:04 +0000

    accept html tags to pass

--- client/src/components/tool-bar/index.jsx
+++ client/src/components/tool-bar/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { capitalize } from '../../utils/HtmlConverter'
 
-const ToolBar = ({children, pageTitle}) => {
+const ToolBar = ({children, pageTitle = null, titleHtml = null}) => {
   return (
     <div className="h-16 flex items-center justify-center mb-8">
-      {pageTitle
+      {pageTitle || titleHtml
         ?      
         <h3 className="mr-3 text-xl text-center">
-          {capitalize(pageTitle)}
+          {pageTitle ? capitalize(pageTitle) : null}
+          {titleHtml}
         </h3>
         :
         null
