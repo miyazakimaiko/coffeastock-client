909bd5ca2f4b91b11b7668ccbf42d4adb1c60217
miyazakimaiko <myzkmik19922@gmail.com>
2022-03-21 01:45:19 +0000

    Create recipe provider

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,16 @@ import App from './App'
 import {AccountProvider} from './context/AccountContext'
 import {AttributeRangeProvider} from './context/AttributeRangeContext'
 import {BeansProvider} from './context/BeansContext'
+import { RecipeProvider } from './context/RecipeContext'
 
 
 ReactDOM.render(
   <AccountProvider>
     <AttributeRangeProvider>
       <BeansProvider>
-        <App/>
+        <RecipeProvider>
+          <App/>
+        </RecipeProvider>
       </BeansProvider>
     </AttributeRangeProvider>
   </AccountProvider>
