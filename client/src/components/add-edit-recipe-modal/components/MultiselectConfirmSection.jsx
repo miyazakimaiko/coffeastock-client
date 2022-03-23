db3bade2d80553635f20423b70a6e1888b442581
miyazakimaiko <myzkmik19922@gmail.com>
2022-03-23 22:41:16 +0000

    Accept content that is not an array

--- client/src/components/add-edit-recipe-modal/components/MultiselectConfirmSection.jsx
+++ client/src/components/add-edit-recipe-modal/components/MultiselectConfirmSection.jsx
@@ -5,11 +5,18 @@ const MultiselectConfirmSection = ({title, content}) => {
     <div className="confirm-section">
       <label className=" mr-4">{title}</label>
       <div className="tag-section font-medium">
-        {content === null ? 
-          <p>-</p> :
+        {content === null 
+            ? 
+          <p>-</p>
+            :
+          Array.isArray(content)
+            ?
           content.map((c) => (
             <span className="text-xs">{c.label}</span>
-        ))}
+          ))
+            :
+          <span className="text-xs">{content.label}</span>
+        }
       </div>
     </div>
   )
