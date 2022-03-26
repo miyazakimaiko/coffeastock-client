128a14caefc24ff9ae9b690f53ab4cf3c09c3cd3
miyazakimaiko <myzkmik19922@gmail.com>
2022-03-26 23:52:55 +0000

    enable to fetch data by user id

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -14,15 +14,29 @@ let validator = [
   body('memo', 'Invalid Memo').escape().isLength({ max: 400 }).optional({ checkFalsy: true }),
   body('extraction_time').custom(value => {
     let valid = String(value).match(/^([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/);
-    if (!valid) {
+    if (valid || value === null) {
+      return true
+    } else {
       throw new Error('Invalid Extraction Time')
     }
-    return true
   })
 ]
 
 module.exports = (app) => {
   const endpoint = process.env.API_ENDPOINT;
+
+    // Get all recipes of a user
+    app.get(endpoint + "/user/:userid/recipes", async (req, res, next) => {
+      try {
+        const results = await db.query(`
+        SELECT * FROM recipes WHERE user_id = $1`, 
+        [req.params.userid]);
+  
+        res.status(200).json(results.rows);
+      } catch (error) {
+        next(error)
+      }
+    });
 
   // Get all recipes of a bean
   app.get(endpoint + "/user/:userid/bean/:productid/recipes", async (req, res, next) => {
