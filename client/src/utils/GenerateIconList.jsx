e006c6c5e56d8f5021c83053625dd58149af88dc
miyazakimaiko <myzkmik19922@gmail.com>
2022-03-28 23:48:49 +0100

    Separate icon list generation functions

---
+++ client/src/utils/GenerateIconList.jsx
@@ -0,0 +1,28 @@
+import StarFullIcon from "../assets/svgs/StarFullIcon";
+import StarHalfIcon from "../assets/svgs/StarHalfIcon";
+import FireFullIcon from "../assets/svgs/FireFullIcon";
+import FireHalfIcon from "../assets/svgs/FireHalfIcon";
+
+export const generateStarIconList = (number) => {
+  const result = []
+  const rounded = Math.ceil(number/10)/2;
+  for (let i = 1; i <= rounded; i ++) {
+    result.push(<StarFullIcon/>)
+  }
+  if (rounded % 1 !== 0) {
+    result.push(<StarHalfIcon />)
+  }
+  return result;
+}
+
+export const generateFireIconList = (number) => {
+  const result = []
+  const rounded = Math.ceil(number)/2;
+  for (let i = 1; i <= rounded; i ++) {
+    result.push(<FireFullIcon/>)
+  }
+  if (rounded % 1 !== 0) {
+    result.push(<FireHalfIcon />)
+  }
+  return result;
+}
