1626d308a3bba390d79cb26e5f77eab34886e7b5
miyazakimaiko <myzkmik19922@gmail.com>
2022-04-05 10:46:36 +0100

    rename method

--- client/src/pages/view-recipe-list/components/RecipeSection.jsx
+++ client/src/pages/view-recipe-list/components/RecipeSection.jsx
@@ -7,6 +7,12 @@ import ChartRadarTaste from './ChartRadarTaste'
 import { useAttributeRangeList } from '../../../context/AttributeRangeContext'
 import { generateStarIconList } from '../../../utils/GenerateIconList'
 import Dropdown from '../../../components/elements/Dropdown'
+import AddEditRecipeModal from '../../../components/add-edit-recipe-modal'
+
+const MODAL_MODE = {
+  EDIT: 'edit',
+  DELETE: 'delete'
+}
 
 const RecipeSection = ({recipe}) => {
   const attributeRangeList = useAttributeRangeList()
@@ -31,7 +37,7 @@ const RecipeSection = ({recipe}) => {
   const [modal, setModal] = useState({mode: null, isOpen: false})
   const [openDetails, setOpenDetails] = useState(false)
 
-  const generateExtractionTime = () => {
+  const parseExtractionTime = () => {
     const extractionTime = Object.keys(extraction_time).map(timeUnit => {
       const unit = timeUnit === 'hours' ? 'hr' : timeUnit === 'minutes' ? 'min' : 'sec';
       return `${extraction_time[timeUnit]} ${unit} `
@@ -39,29 +45,27 @@ const RecipeSection = ({recipe}) => {
     return extractionTime
   }
 
-  console.log('recipe: ', recipe)
-  console.log('attr:', attributeRangeList)
   return (
     <>
       <div className="relative px-4 pt-8 pb-4 mb-4 bg-white shadow-sm rounded-md w-full transition-all">
         <div className="absolute top-2 right-2">
           <Dropdown dropdownText="" type="dot">
             <div className="dropdown-content" >
-              <button type="button" className="dropdown-item" onClick={() => setModal({mode: 'edit', isOpen: true})}>Edit</button>
-              <button type="button" className="dropdown-item" onClick={() => setModal({mode: 'delete', isOpen: true})}>Delete</button>
+              <button type="button" className="dropdown-item" onClick={() => setModal({mode: MODAL_MODE.EDIT, isOpen: true})}>Edit</button>
+              <button type="button" className="dropdown-item" onClick={() => setModal({mode: MODAL_MODE.DELETE, isOpen: true})}>Delete</button>
             </div>
           </Dropdown>
         </div>
 
         <div className="flex">
-          <div className="flex flex-col justify-between w-1/4 h-full px-4 border-r">
-            <p>{brew_date.split('T')[0]}</p>
-            <h3 className="text-xl my-2">{attributeRangeList['method_range']['id-' + method]['label']}</h3>
+          <div className="flex flex-col justify-between w-1/4 h-auto px-4 border-r">
+            <p>{brew_date ? brew_date.split('T')[0] : '-'}</p>
+            <h3 className="text-xl my-2">{method ? attributeRangeList['method_range']['id-' + method]['label'] : '-'}</h3>
             <div className="flex justify-end">
               {total_rate ? generateStarIconList(total_rate) : null} 
             </div>
           </div>
-          <div className="flex flex-col justify-between relative w-1/4 h-full px-4 border-r">
+          <div className="flex flex-col justify-between relative w-1/4 h-auto px-4 border-r">
             <div className="flex justify-between">
               <GiCoffeeBeans className="w-5 h-5 opacity-40"/>
               <p className="text-right mb-4">
@@ -81,7 +85,7 @@ const RecipeSection = ({recipe}) => {
               null}
             </div>
           </div>
-          <div className="flex flex-col justify-between relative w-1/4 h-full px-4 border-r">
+          <div className="flex flex-col justify-between relative w-1/4 h-auto px-4 border-r">
             <div className="flex justify-between">
               <MdWaterDrop className="w-5 h-5 opacity-40" />
               <p className="text-right mb-4">
@@ -101,7 +105,7 @@ const RecipeSection = ({recipe}) => {
               null}
             </div>
           </div>
-          <div className="flex flex-col justify-between relative w-1/4 h-full px-4">
+          <div className="flex flex-col justify-between relative w-1/4 h-auto px-4">
             <div className="flex justify-between">
               <FaCoffee className="w-5 h-5 opacity-40" />
               <p className="text-right mb-4">
@@ -111,7 +115,7 @@ const RecipeSection = ({recipe}) => {
             <div className="flex justify-end flex-wrap">
               {extraction_time 
                   ? 
-              <span className="basic-chip">{generateExtractionTime()}</span> 
+              <span className="basic-chip">{parseExtractionTime()}</span> 
                   : 
               null}            
               {tds 
@@ -159,7 +163,7 @@ const RecipeSection = ({recipe}) => {
                 </div>
                 <div className="coffee-detail-section">
                   <label>Extraction Time: </label>
-                  <p>{extraction_time ? generateExtractionTime() : '-'}</p>
+                  <p>{extraction_time ? parseExtractionTime() : '-'}</p>
                 </div>
                 <div className="coffee-detail-section">
                   <label>Yield Weight: </label>
@@ -199,61 +203,12 @@ const RecipeSection = ({recipe}) => {
         </div>
       </div>
 
-      {showEditModal ? (
-        <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-3xl font-medium">
-                    Modal Title
-                  </h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-medium outline-none focus:outline-none"
-                    onClick={() => setShowEditModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                {/*body*/}
-                <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
-                  </p>
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent px-6 py-2 outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowEditModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowEditModal(false)}
-                  >
-                    Save Changes
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null}
+      {modal.mode === MODAL_MODE.EDIT && modal.isOpen
+          ?
+        <AddEditRecipeModal setModal={setModal} targetRecipe={recipe} mode="edit"/>
+          :
+        null
+      }
     </>
   )
 }
