d2c8db22f6a0e4e64284647a15e9a4fd12118546
miyazakimaiko <myzkmik19922@gmail.com>
2022-04-23 23:40:04 +0100

    New file

---
+++ client/src/utils/myToast.jsx
@@ -0,0 +1,23 @@
+import { toast } from 'react-toastify';
+
+const MODE = {
+  SUCCESS: 'success',
+  ERROR: 'error'
+}
+
+export default function myToast(mode, message) {
+  switch(mode) {
+    case MODE.SUCCESS:
+      return toast.success(message, {
+        position: toast.POSITION.BOTTOM_CENTER
+      })
+    case MODE.ERROR:
+      return toast.error(message, {
+        position: toast.POSITION.BOTTOM_CENTER
+      })
+    default:
+      return toast.success(message, {
+        position: toast.POSITION.BOTTOM_CENTER
+      })
+  }
+}
