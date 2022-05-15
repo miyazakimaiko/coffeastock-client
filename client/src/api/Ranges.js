98f3fe6f8e25e26f3aee9161e9b4baffe3b5c6b4
miyazakimaiko <myzkmik19922@gmail.com>
2022-05-15 12:42:00 +0100

    Add api request method

--- client/src/api/Ranges.js
+++ client/src/api/Ranges.js
@@ -8,6 +8,10 @@ export const getRanges = async (userid) => await api.get(`/user/${userid}/ranges
 
 export const getRange = async (userid, range) => await api.get(`/user/${userid}/range/${range}`).then(res => res.data)
 
-export const editRange = async (userid, range, rangeId, body) => {
-  await api.post(`/user/${userid}/range/${range}/${rangeId}`, body).then(res => res.data)
-}
+export const editRange = async (userid, range, rangeid, body) => await api.post(`/user/${userid}/range/${range}/${rangeid}`, body).then(res => res.data)
+
+export const addRange = async (userid, range, body) => await api.post(`/user/${userid}/range/${range}`, body).then(res => res.data)
+
+export const isRangeInUse = async (userid, range, rangeid) => await api.get(`/user/${userid}/range/${range}/${rangeid}/isinuse`).then(res => res.data)
+
+export const deleteRange = async (userid, range, rangeid) => await api.delete(`/user/${userid}/range/${range}/${rangeid}`).then(res => res.data)
