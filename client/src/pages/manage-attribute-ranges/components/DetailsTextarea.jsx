26636ec3ab0964e0088590f778515e1466c113df
miyazakimaiko <myzkmik19922@gmail.com>
2022-05-29 00:08:22 +0100

    Moved validation inside conponent

--- client/src/pages/manage-attribute-ranges/components/DetailsTextarea.jsx
+++ client/src/pages/manage-attribute-ranges/components/DetailsTextarea.jsx
@@ -1,20 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { capitalize } from '../../../helpers/HtmlConverter'
 
-const DetailsTextarea = ({category, detailsValue, detailsOnChange}) => {
+const DetailsTextarea = ({rangeName, rangeItem, setRangeItem}) => {
+  const [counter, setCounter] = useState(0)
+  const [warning, setWarning] = useState({
+    invalid: false,
+    message: '',
+  });
+
+  const setDetails = (e) => {
+    setRangeItem({...rangeItem, def: e.target.value});
+    setCounter(e.target.value.length);
+
+    if (e.target.value.length > 600) {
+      setWarning({ 
+        ...warning, 
+        invalid: true,
+        message: '* Please enter less than 600 letters.',
+      });
+    }
+    else {
+      setWarning({ 
+        ...warning, 
+        invalid: false,
+        message: '',
+      });
+    }
+  }
+
   return (
     <div className="form-section">
-      <label className="font-medium">{capitalize(category)} Details</label>
+      <label className="font-medium">{capitalize(rangeName)} Details</label>
+      <span className='ml-2 text-red text-xs'>{warning.message}</span>
       <textarea 
         type="text" 
         name="definition"
         autoComplete="off"
         placeholder="Details" 
-        className="blue-outline-transition 
-        block w-full h-32 py-2 px-3 rounded"
-        value={detailsValue}
-        onChange={detailsOnChange}
+        className={
+          `${warning.invalid ? 'red-outline' : 'blue-outline-transition'}  
+          block w-full h-44 py-2 px-3 rounded`
+        }
+        value={rangeItem.def}
+        onChange={setDetails}
       />
+      <div className='w-full mt-1 text-right'>
+        <span>{counter}/600</span>
+      </div>
     </div>
   )
 }
--- client/src/pages/manage-attribute-ranges/components/NameInput.jsx
+++ client/src/pages/manage-attribute-ranges/components/NameInput.jsx
@@ -1,21 +1,53 @@
-import React from 'react'
-import { capitalize } from '../../../helpers/HtmlConverter'
+import React, { useState } from 'react'
+import { capitalize, escapeHtml } from '../../../helpers/HtmlConverter'
 
-const NameInput = ({category, nameValue, nameOnChange, nameWarningText}) => {
+const NameInput = ({rangeName, rangeItem, setRangeItem}) => {
+  const [counter, setCounter] = useState(0);
+  const [warning, setWarning] = useState({
+    invalid: false,
+    message: '',
+  });
+
+  const setName = (name) => {
+    setRangeItem({...rangeItem, label: name});
+    const encodedValue = escapeHtml(name);
+    setCounter(encodedValue.length);
+
+    if (encodedValue.length > 60) {
+      setWarning({ 
+        ...warning, 
+        invalid: true,
+        message: '* Please enter less than 60 letters.',
+      });
+    }
+    else {
+      setWarning({ 
+        ...warning, 
+        invalid: false,
+        message: '',
+      });
+    }
+  }
+
   return (
     <div className="form-section">
-      <label className="font-medium mb-2">{capitalize(category)} Name</label>
+      <label className="font-medium mb-2">{capitalize(rangeName)} Name</label>
+      <span className='ml-2 text-red text-xs'>{warning.message}</span>
       <input
         type="text" 
         name="label"
         autoComplete="off"
-        placeholder={`Name`} 
-        className="blue-outline-transition 
-        block w-full py-2 px-3 rounded"
-        value={nameValue}
-        onChange={nameOnChange}
+        placeholder="Name"
+        className={
+          `${warning.invalid ? 'red-outline' : 'blue-outline-transition'}  
+          block w-full py-2 px-3 rounded`
+        }
+        value={rangeItem.label}
+        onChange={(e) => setName(e.target.value)}
       />
-      <span className="text-xs float-right mt-1">{nameWarningText}</span>
+      <div className='w-full mt-1 text-right'>
+        <span>{counter}/60</span>
+      </div>
     </div>
   )
 }
