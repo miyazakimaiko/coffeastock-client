ac48bd2990f09dd20d69bd13e8d00ca30ef4dbff
miyazakimaiko <myzkmik19922@gmail.com>
2022-08-09 22:44:44 +0100

    Use modalWrapperContainer for delete modal

--- client/src/modals/delete-modal/index.jsx
+++ client/src/modals/delete-modal/index.jsx
@@ -1,6 +1,7 @@
 import { XIcon } from '@heroicons/react/outline'
 import React, { useContext } from 'react'
 import { ModalStateContext } from '../../context/ModalStateContext'
+import ModalWrapperContainer from '../../elements/ModalWrapperContainer';
 
 const DeleteModal = ({label, onDeleteSubmit}) => {
   const {closeModal} = useContext(ModalStateContext);
@@ -8,58 +9,37 @@ const DeleteModal = ({label, onDeleteSubmit}) => {
   // can add all delete functionality here for Bean, Recipe, and Range by using modal.mode
 
   return (
-    <>
-      <div
-        className="justify-center flex overflow-x-hidden 
-        overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">        
-        <div className="relative w-full md:w-550px px-2 my-16 mx-auto">
-          {/*content*/}
-          <div
-            className="border-0 rounded-lg
-            shadow-lg relative flex flex-col 
-            w-full bg-white outline-none focus:outline-none">
-            {/*header*/}
-            <div 
-              className="flex items-center justify-between p-3 rounded-t 
-              border-b border-burnt-sienna border-opacity-20">
-              <button
-                className="p-1 ml-auto border-0 text-black float-right"
-                onClick={closeModal}
-              >
-                <XIcon className="h-6 w-6"/>
-              </button>
-            </div>
-            {/*body*/}
-            <div className="card-content px-3 py-10">
-              <p className='text-center'>
-                Are you sure to delete the entry <strong>{label}</strong> ?
-              </p>
-            </div>
-            <div className="flex items-center justify-between px-8 pb-8">
-              <button
-                className="text-red-500 background-transparent 
-                  px-6 py-2  outline-none 
-                focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={closeModal}
-              >
-                Cancel
-              </button>
-              <button
-                className="bg-red text-white opacity-80 
-                hover:opacity-100    
-                px-6 py-2 rounded-3xl ease-linear transition-all duration-150"
-                type="submit"
-                onClick={onDeleteSubmit}
-              >
-                Yes, Delete
-              </button>
-            </div>
-          </div>
-        </div>
+    <ModalWrapperContainer
+      onCloseClick={closeModal}
+      title="Delete Recipe"
+    >
+      {/*body*/}
+      <div className="card-content px-3 py-10">
+        <p className='text-center'>
+          Are you sure to delete the entry <strong>{label}</strong> ?
+        </p>
       </div>
-      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-    </>
+      <div className="flex items-center justify-between px-8 pb-8">
+        <button
+          className="text-red-500 background-transparent 
+            px-6 py-2  outline-none 
+          focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+          onClick={closeModal}
+        >
+          Cancel
+        </button>
+        <button
+          className="bg-red text-white opacity-80 
+          hover:opacity-100    
+          px-6 py-2 rounded-3xl ease-linear transition-all duration-150"
+          type="submit"
+          onClick={onDeleteSubmit}
+        >
+          Yes, Delete
+        </button>
+      </div>
+    </ModalWrapperContainer>
   )
 }
 
