888e20e0fadd585b4db36dac34916f42d250b194
miyazakimaiko <myzkmik19922@gmail.com>
2022-08-20 19:28:45 +0100

    Refactor: remove unnecessary assignment

--- server/helper/reorderRangeItems.js
+++ server/helper/reorderRangeItems.js
@@ -10,7 +10,6 @@ const reorderRangeListItems = (rangeList) => {
 const reorderRangeItems = (rangeItems) => {
   const orderedRangeItems = Object.entries(rangeItems).sort(sortByName).reduce(
     (obj, [key, value]) => {
-      value['value'] = key; // ID is named as value to enable react-multi-select-component correctly
       obj['id-' + key] = value; // without 'id-' the object is sorted automatially by the id
       return obj;
     }, 
