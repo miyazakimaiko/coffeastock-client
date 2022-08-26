13ac347898e2b437d9719fc349e5f76675a81387
miyazakimaiko <myzkmik19922@gmail.com>
2022-08-27 00:31:47 +0100

    Facilitate two charts

--- client/src/elements/PalateRadarChartDouble.jsx
+++ client/src/elements/PalateRadarChartDouble.jsx
@@ -12,6 +12,7 @@ import { Radar } from 'react-chartjs-2';
 import useRange from '../hooks/useRange';
 import { useUserData } from '../context/AccountContext';
 
+
 ChartJS.register(
   RadialLinearScale,
   PointElement,
@@ -20,7 +21,6 @@ ChartJS.register(
   Tooltip,
   Legend
 );
-
 
 const PalateRadarChartDouble = ({ className, redTitle, blueTitle, redRatesObj, blueRatesObj }) => {
   const userData = useUserData();
@@ -33,32 +33,31 @@ const PalateRadarChartDouble = ({ className, redTitle, blueTitle, redRatesObj, b
   const [radarContent, setRadarContent] = useState(null);
 
   useEffect(() => {
-    if (redRatesObj && blueRatesObj) {
-      const rateCountObj = {};
-      const redRatesIds = Object.keys(redRatesObj);
-      const blueRatesIds = Object.keys(blueRatesObj);
+    if (!Boolean(redRatesObj)) {
+      setRedRates([])
+    }
+  }, [redRatesObj])
 
-      for (let id of redRatesIds) {
-        rateCountObj[id] = 1;
-      }
-      for (let id of blueRatesIds) {
-        rateCountObj[id] = (rateCountObj[id] + 1 || 1);
-      }
+  useEffect(() => {
+    if (!Boolean(blueRatesObj)) {
+      setBlueRates([])
+    }
+  }, [blueRatesObj])
 
-      const idsSortedByCount = Object.keys(rateCountObj).sort(function(a, b) {
-          if (rateCountObj[a] < rateCountObj[b]) return 1;
-          else if (rateCountObj[a] > rateCountObj[b]) return -1;
-          else return 0;
-        });
-
-      const redRates = Array(idsSortedByCount.length).fill(0);
-      const blueRates = Array(idsSortedByCount.length).fill(0);
-      
-      for (let i=0; i<idsSortedByCount.length; i++) {
-        const id = idsSortedByCount[i];
-        redRates[i] = (redRatesObj[id] || null);
-        blueRates[i] = (blueRatesObj[id] || null);
-      }
+  useEffect(() => {
+    if (redRatesObj && !blueRatesObj) {
+      setRedRates(Object.values(redRatesObj));
+      setLabels(Object.keys(redRatesObj).map(id => palateRange[`id-${id}`].label))
+    }
+    else if (!redRatesObj && blueRatesObj) {
+      setBlueRates(Object.values(blueRatesObj));
+      setLabels(Object.keys(blueRatesObj).map(id => palateRange[`id-${id}`].label))
+    }
+    else if (redRatesObj && blueRatesObj) {
+      const rateCountObj = makeRateCountObj(redRatesObj, blueRatesObj);
+      const idsSortedByCount = makeSortedRateIdsByCount(rateCountObj);
+      const [redRates, blueRates] = 
+        makeSortedRatesByCount(idsSortedByCount, redRatesObj, blueRatesObj)
 
       setRedRates(redRates);
       setBlueRates(blueRates);
@@ -66,36 +65,72 @@ const PalateRadarChartDouble = ({ className, redTitle, blueTitle, redRatesObj, b
     }
   }, [redRatesObj, blueRatesObj]);
 
+  function makeRateCountObj(redRatesObj, blueRatesObj) {
+    const rateCountObj = {};
+    const redRatesIds = Object.keys(redRatesObj);
+    const blueRatesIds = Object.keys(blueRatesObj);
+
+    for (let id of redRatesIds) {
+      rateCountObj[id] = 0.5;
+    }
+    for (let id of blueRatesIds) {
+      rateCountObj[id] = (rateCountObj[id] + 1 || 1);
+    }
+    return rateCountObj;
+  }
+
+  function makeSortedRateIdsByCount(rateCountObj) {
+    return Object.keys(rateCountObj).sort(function(a, b) {
+      if (rateCountObj[a] < rateCountObj[b]) return 1;
+      else if (rateCountObj[a] > rateCountObj[b]) return -1;
+      else return 0;
+    });
+  }
+
+  function makeSortedRatesByCount(idsSortedByCount, redRatesObj, blueRatesObj) {
+    const redRates = Array(idsSortedByCount.length).fill(0);
+    const blueRates = Array(idsSortedByCount.length).fill(0);
+    
+    for (let i=0; i<idsSortedByCount.length; i++) {
+      const id = idsSortedByCount[i];
+      redRates[i] = (redRatesObj[id] || null);
+      blueRates[i] = (blueRatesObj[id] || null);
+    }
+    return [redRates, blueRates]
+  }
+
   useEffect(() => {
     if (labels && redRates && blueRates) {
-      const data = {
-        labels: labels,
-        datasets: [
-          {
-            label: redTitle,
-            data: redRates,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1,
-          },
-          {
-            label: blueTitle,
-            data: blueRates,
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgb(54, 162, 235)',
-            borderWidth: 1,
-          },
-        ],
-      };
-      const options = {
-        scale: {
-          min: 0,
-          max: 10,
-        },
-      };
-      setRadarContent(<Radar data={data} options={options} />);
+      setDoubleRadarChartContent(labels, redRates, blueRates)
     }
   }, [labels, redRates, blueRates])
+
+  function setDoubleRadarChartContent(labels, redRates, blueRates) {
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: redTitle,
+          data: redRates,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1,
+        },
+        {
+          label: blueTitle,
+          data: blueRates,
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgb(54, 162, 235)',
+          borderWidth: 1,
+        },
+      ],
+    };
+    const options = {
+      scale: { min: 0, max: 10,
+      },
+    };
+    setRadarContent(<Radar data={data} options={options} />);
+  }
 
   if (palateRangeIsLoading) {
     return 'Loading...'
