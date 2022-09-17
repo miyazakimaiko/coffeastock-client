669b1ec1da95c4d76dc6c336bd09e9ab8716e582
miyazakimaiko <myzkmik19922@gmail.com>
2022-09-17 11:58:58 +0100

    Fix select statement error

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,12 +8,13 @@ module.exports = (app) => {
 
   app.get(endpoint + "/user", async (req, res, next) => {
     try {
-      const userExists = await db.query(`
-        SELECT 1 FROM USERS WHERE user_id = $1
+      const result = await db.query(`
+        SELECT COUNT(*) FROM USERS WHERE user_id = $1
         `, 
         [req.query.id]
       );
-      if (userExists) res.status(200).json(true);
+      
+      if (result.rows[0].count === '1') res.status(200).json(true);
       else res.status(200).json(false);
     }
      catch (err) {
@@ -33,6 +34,7 @@ module.exports = (app) => {
         INSERT INTO
         USERS (
           user_id,
+          user_type,
           unit_solid_weight_id,
           unit_fluid_weight_id,
           unit_temperature_id,
@@ -47,23 +49,24 @@ module.exports = (app) => {
           palate_range,
           aroma_range
         )
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
         `,
         [
           req.params.userid,                        // 1
-          1,                                        // 2
+          'TRIAL',                                  // 2
           1,                                        // 3
           1,                                        // 4
-          JSON.stringify(req.body.origin_range),    // 5
-          JSON.stringify(req.body.farm_range),      // 6
-          JSON.stringify(req.body.variety_range),   // 7
-          JSON.stringify(req.body.process_range),   // 8
-          JSON.stringify(req.body.roaster_range),   // 9
-          JSON.stringify(req.body.method_range),    // 10
-          JSON.stringify(req.body.water_range),     // 11
-          JSON.stringify(req.body.grinder_range),   // 12
-          JSON.stringify(req.body.palate_range),    // 13
-          JSON.stringify(req.body.aroma_range)      // 14
+          1,                                        // 5
+          JSON.stringify(req.body.origin_range),    // 6
+          JSON.stringify(req.body.farm_range),      // 7
+          JSON.stringify(req.body.variety_range),   // 8
+          JSON.stringify(req.body.process_range),   // 9
+          JSON.stringify(req.body.roaster_range),   // 10
+          JSON.stringify(req.body.method_range),    // 11
+          JSON.stringify(req.body.water_range),     // 12
+          JSON.stringify(req.body.grinder_range),   // 13
+          JSON.stringify(req.body.palate_range),    // 14
+          JSON.stringify(req.body.aroma_range)      // 15
         ]
       );
       res.status(200).json('User ranges are set.');
@@ -107,10 +110,27 @@ module.exports = (app) => {
     }
   });
 
-  app.get(endpoint + "/user/:userid/unit-ids", async (req, res, next) => {
+  // get how many MB the user use in total.
+  app.get(endpoint + "/user/:userid/total-used-mb", async (req, res, next) => {
+    try {
+
+      const totalSize = await db.query(
+        `SELECT * FROM get_user_total_used_mb($1)`, 
+        [req.params.userid]
+      );
+
+      res.status(200).json(totalSize.rows[0].get_user_total_used_mb);
+
+    } catch(err) {
+      next(err);
+    }
+  });
+
+  app.get(endpoint + "/user/:userid/info", async (req, res, next) => {
     try {
       const result = await db.query(`
         SELECT 
+          user_type,
           unit_solid_weight_id, 
           unit_fluid_weight_id,
           unit_temperature_id
