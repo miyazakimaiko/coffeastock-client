4133f04eba407ecb7d4712a77d39c0be5a7fd8d3
miyazakimaiko <myzkmik19922@gmail.com>
2022-09-22 22:41:52 +0100

    New spinner

---
+++ client/src/elements/SpinnerSmall.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CgSpinner } from 'react-icons/cg'
+
+const SpinnerSmall = () => {
+  return (
+    <>
+      <CgSpinner className="spinner w-5 h-5 inline-block" />
+    </>
+  )
+}
+
+export default SpinnerSmall
