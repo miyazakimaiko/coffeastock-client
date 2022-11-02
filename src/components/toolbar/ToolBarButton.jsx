3f37f9a497f0263a477a1a63c6c1ba254b00d0aa
miyazakimaiko <myzkmik19922@gmail.com>
2022-11-02 20:09:00 +0000

    changed to test runner

--- src/components/toolbar/ToolBarButton.jsx
+++ src/components/toolbar/ToolBarButton.jsx
@@ -1,6 +1,6 @@
-import { PlusIcon } from '@heroicons/react/outline'
-import React from 'react'
-import { capitalize } from '../../helpers/HtmlConverter'
+import { PlusIcon } from '@heroicons/react/outline';
+import React from 'react';
+import { capitalize } from '../../helpers/HtmlConverter';
 
 const ToolBarButton = ({title, onClick}) => {
   return (
