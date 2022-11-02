4317cebf199c29f48c4650059fbf25cf9058f543
miyazakimaiko <myzkmik19922@gmail.com>
2022-11-02 22:09:54 +0000

    stringify ids

--- src/utils/UserPool.jsx
+++ src/utils/UserPool.jsx
@@ -3,8 +3,8 @@ import { CognitoUserPool } from 'amazon-cognito-identity-js';
 require("dotenv").config();
 
 const poolData = {
-  UserPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
-  ClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
+  UserPoolId: `${process.env.REACT_APP_COGNITO_USER_POOL_ID}`,
+  ClientId: `${process.env.REACT_APP_COGNITO_CLIENT_ID}`,
 }
 
 export default new CognitoUserPool(poolData);
