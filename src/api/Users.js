892270703e4b4fa3038211df648a151790152044
miyazakimaiko <myzkmik19922@gmail.com>
2022-11-06 16:51:27 +0000

    add log

--- src/api/Users.js
+++ src/api/Users.js
@@ -3,7 +3,23 @@ import axios from './CustomAxios'
 export const findUser = async (userid, token) => {
   return await axios.get(`/user`, { headers: { accesstoken: token }, params: { id: userid } })
                     .then(res => res.data)
-                    .catch(err => err);
+                    .catch(error => {
+                      if (error.response) {
+                        // The request was made and the server responded with a status code
+                        // that falls out of the range of 2xx
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                        console.log(error.response.headers);
+                      } else if (error.request) {
+                        // The request was made but no response was received
+                        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                        // http.ClientRequest in node.js
+                        console.log(error.request);
+                      } else {
+                        // Something happened in setting up the request that triggered an Error
+                        console.log('Error', error.message);
+                      }
+                    });
 }
 
 export const getUser = async (userid, token) => {
